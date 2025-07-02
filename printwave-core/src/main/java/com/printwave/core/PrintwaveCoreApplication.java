f9e248c767badfa1b0becedb3e0beda4d3743728
sdragon <atharvawakodikar699@gmail.com>
2025-07-02 22:16:10 +0530

    init printwave-core with Spring Boot setup

---
+++ printwave-core/src/main/java/com/printwave/core/PrintwaveCoreApplication.java
@@ -0,0 +1,13 @@
+package com.printwave.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintwaveCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintwaveCoreApplication.class, args);
+	}
+
+}
---
+++ printwave-core/src/test/java/com/printwave/core/PrintwaveCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printwave.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintwaveCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
