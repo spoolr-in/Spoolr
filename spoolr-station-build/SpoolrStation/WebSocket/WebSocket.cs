a597ed5ae7f5fa6e1b7727a0bd5062bb3c0683e1
VyankateshKulkarni_13 <vdkulkarni17@gmail.com>
2025-11-07 17:24:05 +0530

    "chore:_remove_spoolr-station-build_from_git"

--- spoolr-station-build/SpoolrStation/WebSocket/WebSocket.cs
+++
@@ -1,8 +0,0 @@
-// Global using statements to consolidate WebSocket namespace access
-global using SpoolrStation.WebSocket.Models;
-global using SpoolrStation.WebSocket.Services;
-
-namespace SpoolrStation.WebSocket
-{
-    // This file provides a consolidated namespace entry point for the WebSocket functionality
-}
